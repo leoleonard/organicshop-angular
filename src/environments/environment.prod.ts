2f953b048d77a40334681e765c5bf8e000a30bc5
Damian Juzyk <damian.juzyk@gmail.com>
2018-04-17 19:22:03 +0200

    add basic config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDSXK7mK6sTIOVg6tXSpsrqKfTrcMw742c",
+    authDomain: "organic-shop-39881.firebaseapp.com",
+    databaseURL: "https://organic-shop-39881.firebaseio.com",
+    projectId: "organic-shop-39881",
+    storageBucket: "organic-shop-39881.appspot.com",
+    messagingSenderId: "88319152907"
+}
+};
